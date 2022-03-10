8eb502b584d6677201465df1417ad04951a49e3a
ThiagoSilvaMorato <thiagosmorato@gmail.com>
2022-03-10 15:33:41 -0300

    Feito o input pra busca

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+//Modal para o search input
+export function SearchInput() {
+  return (
+    <div className='container'>
+      <input></input>
+      <button>Search</button>
+    </div>
+  );
+}
