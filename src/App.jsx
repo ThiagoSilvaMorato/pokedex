5af04bfd8c5696e5ca87d4fb96076278245ba022
ThiagoSilvaMorato <thiagosmorato@gmail.com>
2022-03-23 10:41:09 -0300

    Criado componente 'Badge'

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import { Header } from "./components/Header/";
 // import { PokePhoto } from "./components/PokePhoto/";
 // import { Footer } from "./components/Footer";
-import { SquareBadge } from "./components/SquareBadge";
+// import { SquareBadge } from "./components/SquareBadge";
+import { Badge } from "./components/Badge";
 
 import "./index.css";
 
@@ -13,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' exact element={<SquareBadge />} />
+        <Route path='/' exact element={<Badge />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Badge/index.jsx
@@ -0,0 +1,9 @@
+import "./style.scss";
+
+export function Badge() {
+  return (
+    <div className='badge fairy'>
+      <p>Normal</p>
+    </div>
+  );
+}
